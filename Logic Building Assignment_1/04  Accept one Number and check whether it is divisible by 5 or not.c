0b1d010c8e229415b77094496353e55d1e3665f9
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-04 20:24:59 +0530

    First Assignment

---
+++ Logic Building Assignment_1/04  Accept one Number and check whether it is divisible by 5 or not.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<conio.h>
+
+typedef int BOOL;
+#define TRUE 1
+#define FALSE 0
+
+BOOL Check(int iNo)
+{
+     if((iNo%5)==0)
+     {
+         return TRUE;
+     }
+     else
+     {
+         return FALSE;
+     }
+}
+
+int main()
+{
+    int iValue=0;
+    BOOL bRet=FALSE;
+
+    printf("\n Enter Number = ");
+    scanf("%d",&iValue);
+
+    bRet=Check(iValue);
+
+    if(bRet==TRUE)
+     {
+        printf("\n Divisible by 5");
+     }
+    else
+    {
+       printf("\n Not Divisible by 5");
+    }
+    getch();
+
+    return 0;
+}
