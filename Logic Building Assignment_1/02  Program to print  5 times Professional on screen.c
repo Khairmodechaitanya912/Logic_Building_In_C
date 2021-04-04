bc60b50fda1ba63c4c87376d14e6423bd67813b3
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-04 20:19:05 +0530

    First Assignment

---
+++ Logic Building Assignment_1/02  Program to print  5 times Professional on screen.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Display()
+{
+    int i=0;
+
+    for(i=1;i<=5;i++)
+    {
+        printf("\n Professional...! \n");
+    }
+    return ;
+}
+
+int main()
+{
+    Display();
+
+    getch();
+
+    return 0;
+
+}
