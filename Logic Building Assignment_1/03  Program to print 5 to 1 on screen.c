90a6dc0b41fe60d41785d798bdf222d333b75ef9
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-04 20:22:14 +0530

    First Assignment

---
+++ Logic Building Assignment_1/03  Program to print 5 to 1 on screen.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Display()
+{
+    int i=5;
+
+    while(i>=1)
+    {
+        printf(" %d \n",i);
+
+        i--;
+    }
+    return ;
+}
+
+int main()
+{
+    Display();
+
+    getch();
+
+    return 0;
+
+}
