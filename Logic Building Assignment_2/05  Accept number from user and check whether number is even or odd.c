544558c9e43ae0be2adc0181cf4d9d6f045bf338
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-05 22:48:30 +0530

    Second Assignment

---
+++ Logic Building Assignment_2/05  Accept number from user and check whether number is even or odd.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL ChkEven(int iNo)
+{
+     if(iNo%2==0)
+    {
+       return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+
+}
+int main()
+{
+    int iValue=0;
+    BOOL bRet= FALSE;
+
+    printf("\n Enter Number = ");
+    scanf("%d",&iValue);
+
+    bRet=ChkEven(iValue);
+
+    if(bRet==TRUE)
+    {
+        printf("\n Number is Even...!\n ");
+    }
+    else
+    {
+        printf("\n Number is Odd...!\n ");
+    }
+
+    getch();
+    return 0;
+}
