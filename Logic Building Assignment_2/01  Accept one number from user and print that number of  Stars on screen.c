f85777e20420f8c0af51c68f180b5d183f76ba61
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-05 22:43:40 +0530

    Second Assignment

---
+++ Logic Building Assignment_2/01  Accept one number from user and print that number of  Stars on screen.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Display(int iNo)
+{
+    int iCnt=0;
+    while(iCnt<iNo)
+    {
+        printf("\n * ");
+        iCnt++;
+    }
+    return ;
+}
+
+int main()
+{
+    int iValue=0;
+
+    printf("Enter Number = ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    getch();
+    return 0;
+}
+
