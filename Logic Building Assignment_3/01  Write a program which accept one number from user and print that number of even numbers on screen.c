ddb7f7bd14aedd0819180bfb1185ab3011bb408e
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-06 22:35:30 +0530

    Third Assignment

---
+++ Logic Building Assignment_3/01  Write a program which accept one number from user and print that number of even numbers on screen.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+void PrintEven(int iNo)
+{
+    int i=0;
+    if(iNo<=0)
+    {
+        return ;
+    }
+    for(i=1;i<=iNo*2;i++)
+    {
+        if(i%2==0)
+        {
+            printf("\n %d",i);
+        }
+    }
+    printf("\n");
+    return ;
+}
+int main()
+{
+    int iValue=0;
+
+    printf("\n Enter Number = ");
+    scanf("%d",&iValue);
+
+    PrintEven(iValue);
+
+    getch();
+    return 0;
+}
