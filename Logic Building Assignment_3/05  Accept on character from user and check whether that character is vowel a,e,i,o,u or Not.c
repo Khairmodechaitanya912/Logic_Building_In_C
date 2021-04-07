af8432af0c8dee56df33ec451ecf1217477442b8
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-07 17:21:55 +0530

    Third Assignment

---
+++ Logic Building Assignment_3/05  Accept on character from user and check whether that character is vowel a,e,i,o,u or Not.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<conio.h>
+
+typedef int BOOL;
+
+#define TRUE 1
+#define FALSE 0
+
+BOOL ChkVowel(char CValue)
+{
+   if(CValue=='A'||CValue=='a'||CValue=='E'||CValue=='e'||CValue=='I'||CValue=='i'||CValue=='O'||CValue=='o'||CValue=='U'||CValue=='u')
+   {
+       return TRUE;
+   }
+   else
+   {
+       return FALSE;
+   }
+}
+int main()
+{
+    char cValue='\0';
+    BOOL bRet=FALSE;
+
+    printf("\n Enter Character = ");
+    scanf("%c",&cValue);
+
+    bRet=ChkVowel(cValue);
+
+    if(bRet==TRUE)
+    {
+        printf("\n It is Vowel...!\n ");
+    }
+    else
+    {
+        printf("\n It is not Vowel...!\n ");
+    }
+
+    getch();
+    return 0;
+}
