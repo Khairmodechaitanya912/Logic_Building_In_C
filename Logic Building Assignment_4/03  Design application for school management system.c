28471f72726b67b13e3b9769a2e34eb4e02916ea
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-09 22:28:05 +0530

    Fourth Assignment

---
+++ Logic Building Assignment_4/03  Design application for school management system.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+
+void School_Std(int iStd)
+{
+    if(iStd==1)
+    {
+        printf("\n Total Fee Of First Standard is :- 8900 \n");
+    }
+    else if(iStd==2)
+    {
+        printf("\n Total Fee Of Second Standard is :- 12790 \n");
+    }
+    else if(iStd==3)
+    {
+        printf("\n Total Fee Of Third Standard is :- 21000 \n");
+    }
+    else
+    {
+        printf("\n Total Fee Of Fourth Standard is :- 23450 \n");
+    }
+}
+int main()
+{
+    int std;
+    up:
+    printf("\n Enter School standard = ");
+    scanf("%d",&std);
+
+    if(std>4||std<1)
+    {
+        printf("\n Plzz Enter Valid Standard ...!!\n");
+        goto up;
+    }
+
+    School_Std(std);
+
+    getch();
+    return 0;
+}
