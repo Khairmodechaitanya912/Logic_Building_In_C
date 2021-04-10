c1b35cabb8532be2dc84a716a6f2235e77c1b7c4
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-10 23:14:52 +0530

    Fifth Assignment

---
+++ Logic Building Assignment_5/05  WAP which accept number from user and return difference between summation of all its factors and non factors.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+
+int Fact_Diff(int iNo)
+{
+    int i=0,NfSum=0,FSum=0;
+
+    if(iNo<=0)
+    {
+        iNo=-iNo;
+    }
+
+    for(i=1;i<iNo;i++)
+    {
+        if(iNo%i!=0)
+        {
+            NfSum=NfSum+i;
+        }
+        else
+        {
+            FSum=FSum+i;
+        }
+    }
+    return (FSum-NfSum);
+}
+int main()
+{
+    int iValue=0;
+    int iRet=0;
+
+    printf("\n Enter Number = ");
+    scanf("%d",&iValue);
+
+    iRet=Fact_Diff(iValue);
+
+    printf("\n Difference Between Sum of all its Factors and Non factors is %d \n",iRet);
+
+    getch();
+    return 0;
+}
