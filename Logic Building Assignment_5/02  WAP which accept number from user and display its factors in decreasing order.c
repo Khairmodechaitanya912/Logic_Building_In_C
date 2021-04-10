5b736c9cf339fd4ac94a6940a208736eec2a0380
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-10 23:11:31 +0530

    Fifth Assignment

---
+++ Logic Building Assignment_5/02  WAP which accept number from user and display its factors in decreasing order.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+int Fact_Rev(int iNo)
+{
+    int i=0;
+
+    if(iNo<=0)
+    {
+        iNo=-iNo;
+    }
+
+    printf("\n %d Number of Factor are Decreasing in order : ",iNo);
+
+    for(i=iNo/2;i>=1;i--)
+    {
+        if(iNo%i==0)
+        {
+           printf("\n\n %d",i);
+        }
+    }
+    return ;
+}
+int main()
+{
+    int iValue=0;
+
+    printf("\n Enter Number = ");
+    scanf("%d",&iValue);
+
+    Fact_Rev(iValue);
+
+    getch();
+    return 0;
+}
