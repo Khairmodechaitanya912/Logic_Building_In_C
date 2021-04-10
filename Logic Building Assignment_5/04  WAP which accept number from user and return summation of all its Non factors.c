e65ee087769a754f8c223746b0776d0b20d987d5
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-10 23:13:35 +0530

    Fifth Assignment

---
+++ Logic Building Assignment_5/04  WAP which accept number from user and return summation of all its Non factors.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+
+int Sum_Non_Fact(int iNo)
+{
+    int i=0,Sum=0;
+
+    if(iNo<=0)
+    {
+        iNo=-iNo;
+    }
+
+    for(i=1;i<=iNo;i++)
+    {
+        if(iNo%i!=0)
+        {
+            Sum=Sum+i;
+        }
+    }
+    return Sum;
+}
+int main()
+{
+    int iValue=0;
+    int iRet=0;
+
+    printf("\n Enter Number = ");
+    scanf("%d",&iValue);
+
+    iRet=Sum_Non_Fact(iValue);
+
+    printf("Summation of all its Non Factors is %d\n ",iRet);
+
+    getch();
+    return 0;
+}
