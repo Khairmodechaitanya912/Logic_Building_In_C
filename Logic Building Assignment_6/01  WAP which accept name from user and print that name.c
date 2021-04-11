7998404fe6251c302e1c3e9404af34c991913ba1
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-11 21:34:45 +0530

    Sixth Assignment

---
+++ Logic Building Assignment_6/01  WAP which accept name from user and print that name.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char Name[30];
+
+    printf("\n Enter Your Full Name = ");
+    scanf("%s",&Name);
+
+    printf("\n Your Name is :- %s%s ",Name);
+
+    getch();
+    return 0;
+}
