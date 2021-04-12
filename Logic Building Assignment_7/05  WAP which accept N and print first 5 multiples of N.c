758251247c3a995900b9f358b0d92fcf4ec2995d
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-12 17:46:29 +0530

    Seventh Assignment

---
+++ Logic Building Assignment_7/05  WAP which accept N and print first 5 multiples of N.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Multiple_Display(int iNo)
+{
+    int i=0,Mult=0;
+
+    for(i=1;i<=5;i++)
+    {
+        Mult=iNo*i;
+        printf("\n %d",Mult);
+    }
+
+    printf("\n");
+
+    return ;
+}
+int main()
+{
+    int iValue=0;
+
+    printf("\n Enter Number = ");
+    scanf("%d",&iValue);
+
+    Multiple_Display(iValue);
+
+    getch();
+    return 0;
+}
