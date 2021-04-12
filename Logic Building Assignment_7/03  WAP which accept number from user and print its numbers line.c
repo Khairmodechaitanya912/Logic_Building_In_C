ee4af2b1678c545a1e8e1ed5c29c99aee62f2d5c
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-12 17:44:22 +0530

    Seventh Assignment

---
+++ Logic Building Assignment_7/03  WAP which accept number from user and print its numbers line.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Display(int iNo)
+{
+    int i=0;
+
+    for(i=-iNo;i<=iNo;i++)
+    {
+        printf(" %d",i);
+    }
+    printf("\n");
+
+    return ;
+}
+int main()
+{
+    int iValue=0;
+
+    printf("\n Enter Number = ");
+    scanf("%d",&iValue);
+
+    printf("\n");
+
+    Display(iValue);
+
+    getch();
+    return 0;
+}
