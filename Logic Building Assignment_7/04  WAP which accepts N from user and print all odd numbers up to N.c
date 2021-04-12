3a3b368f0b533a85f82514e5b3ebd7123e73464b
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-12 17:45:21 +0530

    Seventh Assignment

---
+++ Logic Building Assignment_7/04  WAP which accepts N from user and print all odd numbers up to N.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Odd_Display(int iNo)
+{
+    int i=0,ONo=0;
+    for(i=1;i<=iNo;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("\n %d",i);
+        }
+    }
+
+    printf("\n");
+
+    return ;
+}
+int main()
+{
+    int iValue=0;
+
+    printf("\n Enter Number = ");
+    scanf("%d",&iValue);
+
+    Odd_Display(iValue);
+
+    getch();
+    return 0;
+}
