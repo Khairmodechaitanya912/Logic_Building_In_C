b1bcb9d51c595da82c2d50b1f702f8a083eeae8d
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-13 18:30:51 +0530

    Eighth Assignment

---
+++ Logic Building Assignment_8/01  WAP to check number is greater ,medium,less.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Number(int iNo)
+{
+    if(iNo<=50)
+    {
+        printf("\n Number is Small...!!\n");
+    }
+    else if(iNo>50&&iNo<100)
+    {
+        printf("\n Number is Medium...!!\n");
+    }
+    else
+    {
+        printf("\n Number is Large...!!\n");
+    }
+
+    return ;
+}
+int main()
+{
+    int iValue=0;
+
+    printf("\n Enter Number = ");
+    scanf("%d",&iValue);
+
+    Number(iValue);
+
+    getch();
+    return 0;
+}
