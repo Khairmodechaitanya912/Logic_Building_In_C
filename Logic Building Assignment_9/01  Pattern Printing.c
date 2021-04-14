0a7bd6cb1aab58326b0e9cb9e907bda0a4f624ea
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-14 22:45:35 +0530

    Ninth Assignment

---
+++ Logic Building Assignment_9/01  Pattern Printing.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Display(int iNo)
+{
+   int i=1,j=1;
+
+   if(iNo<0)
+   {
+       iNo=-iNo;
+   }
+
+   for(i=1;i<=iNo;i++)
+   {
+       printf(" * ");
+   }
+
+   for(j=1;j<=iNo;j++)
+   {
+       printf(" # ");
+   }
+
+   printf("\n");
+
+   return ;
+}
+int main()
+{
+    int iValue=0;
+
+    printf("\n Enter Number = ");
+    scanf("%d",&iValue);
+
+    printf("\n");
+
+    Display(iValue);
+
+    getch();
+    return 0;
+}
