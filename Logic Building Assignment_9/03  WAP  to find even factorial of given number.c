c2ac07c1742fc36a091d910bb2f1acaafc32828a
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-14 22:56:33 +0530

    Ninth Assignment

---
+++ Logic Building Assignment_9/03  WAP  to find even factorial of given number.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+int Even_Factorial(int iNo)
+{
+  int fact=1;
+
+   if(iNo<0)
+   {
+       iNo=-iNo;
+   }
+   for(;iNo>0;iNo--)
+   {
+     if(iNo%2==0)
+     {
+         fact=fact*iNo;
+     }
+   }
+   return fact;
+}
+int main()
+{
+    int iValue=0,iRet=0;
+
+    printf("\n Enter Number = ");
+    scanf("%d",&iValue);
+
+    iRet=Even_Factorial(iValue);
+
+    printf("\n Even Factorial of Given Number is %d \n ",iRet);
+
+    getch();
+    return 0;
+}
