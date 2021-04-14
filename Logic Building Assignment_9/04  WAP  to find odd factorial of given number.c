0487cc02abeb90e18fd106f379a326990e77ca79
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-04-14 22:57:36 +0530

    Ninth Assignment

---
+++ Logic Building Assignment_9/04  WAP  to find odd factorial of given number.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+int Odd_Factorial(int iNo)
+{
+    int Ofact=1;
+
+    if(iNo<0)
+    {
+        iNo=-iNo;
+    }
+
+    for(;iNo>0;iNo--)
+    {
+        if(iNo%2!=0)
+        {
+            Ofact=Ofact*iNo;
+        }
+    }
+    return Ofact;
+}
+int main()
+{
+    int iValue=0,iRet=0;
+
+    printf("\n Enter Number = ");
+    scanf("%d",&iValue);
+
+    iRet=Odd_Factorial(iValue);
+
+    printf("\n Odd Factorial of Given Number is %d \n ",iRet);
+
+    getch();
+    return 0;
+}
