2f8ab425991bdda22e2e78e36ca2edea1cfb034a
Chaitanya <chaitanyakhairmode912@gmail.com>
2021-05-06 21:57:11 +0530

    Eighteenth Assignment

---
+++ Logic Building Assignment_18/02  Accept number of rows and number of columns from user and display below pattern.c
@@ -0,0 +1,64 @@
+/*
+
+        Input : iRow = 4 iCol = 4
+
+        Output : * * * *
+                 * * * #
+                 * * # #
+                 * # # #
+
+ */
+
+
+#include<stdio.h>
+#include<conio.h>
+
+void Pattern(int iRow , int iCol)
+{
+    int i=0 , j=0 ;
+
+    printf("\n");
+
+    for(i=0;i<iRow;i++)
+    {
+
+        for(j=0;j<iCol;j++)
+        {
+
+            if(j>=(iCol-i)&&i>0)
+            {
+
+                printf(" # ");
+
+            }
+            else
+            {
+
+                printf(" * ");
+
+            }
+        }
+
+        printf("\n");
+    }
+
+    return ;
+}
+
+int main()
+{
+
+    int iValue1=0 , iValue2=0 ;
+
+    printf("\n Enter Number of Rows = ");
+    scanf("%d",&iValue1);
+
+    printf("\n Enter Number of Columns = ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1 , iValue2);
+
+    getch();
+    return 0;
+
+}
